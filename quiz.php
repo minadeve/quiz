c0f710e8c8adc71ffa0b0fd8a26bbe9d890784d3
minadeve <gholamreza.habibi42@gmail.com>
2024-12-18 09:00:03 +0100

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,7 +1,7 @@
 <?php
 //connect to db full Tube
 $servername = "localhost";
-$username = "rsosot";
+$username = "root";
 $password = "";
 $dbname = "blsosp";
 //open connection for doing somthings in code
