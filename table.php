2fbc99702d6be8fb79f4492e92f3e8cd56b2841d
minadeve <gholamreza.habibi42@gmail.com>
2025-01-02 09:37:46 +0100

    Update table.php

--- table.php
+++ table.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-5">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Random 10x200 Table</title>
     <style>
