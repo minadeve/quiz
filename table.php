a09baee32801d9b4ce246bfd4433fdc6ccebf74d
minadeve <gholamreza.habibi42@gmail.com>
2025-01-08 08:20:02 +0100

    Update table.php

--- table.php
+++ table.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random 130x200 Table</title>
+    <title>Random 230x200 Table</title>
     <style>
         table {
             border-collapse: collapse;
