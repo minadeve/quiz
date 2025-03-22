60f7e9fa8f758d17d6d9e1bc5258a2d997bead3b
minadeve <gholamreza.habibi42@gmail.com>
2025-03-22 10:48:45 +0100

    Update table.php

--- table.php
+++ table.php
@@ -7,7 +7,7 @@
     <style>
         table {
             border-collapse: collapse;
-            width: 20%;
+            width: 30%;
             margin: 15px auto;
             text-align: center;
         }
