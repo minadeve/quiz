6f1a4f3a898a07dcb03f502cad85e0205c7143ba
minadeve <gholamreza.habibi42@gmail.com>
2025-04-12 12:25:07 +0200

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,7 +1,7 @@
 <?php
 //connect to dbiZZ full Tubee
 $servername = "localhost";
-$username = "rootSSS";
+$username = "root1";
 $password = "";
 $dbname = "blsp";
 //open connection for doing somthings in code
